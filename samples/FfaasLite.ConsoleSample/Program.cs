079ffbc26e27629c32bef7d299ce559be776703f
Dmitry [KP0H™] Pelevin <dmitry@pelevin.pro>
2025-09-24 01:35:38 +0300

    Console SDK usage example

---
+++ samples/FfaasLite.ConsoleSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using FfaasLite.Core.Flags;
+using FfaasLite.SDK;
+
+var client = new FlagClient("http://localhost:8080");
+
+var ctx = new EvalContext(
+    UserId: "u3",
+    Attributes: new Dictionary<string, string>
+    {
+        ["country"] = "NL",
+        ["email"] = "test@corp.com"
+    }
+);
+
+var result = await client.EvaluateAsync("ui-ver", ctx);
+
+Console.WriteLine($"Flag '{result.Key}' evaluated:");
+Console.WriteLine($"  Value   : {result.Value}");
+Console.WriteLine($"  Variant : {result.Variant}");
+Console.WriteLine($"  Type    : {result.Type}");
+
+result = await client.EvaluateAsync("new-ui", ctx);
+Console.WriteLine($"Flag '{result.Key}' evaluated:");
+if (result.AsBool() == true)
+{
+    Console.WriteLine("New UI enabled!");
+}
+else
+{
+    Console.WriteLine("Using old UI");
+}
+Console.ReadKey();
