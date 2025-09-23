c256a787e98b16ee5726bc004060907b81ae461a
Dmitry [KP0H™] Pelevin <dmitry@pelevin.pro>
2025-09-23 20:49:35 +0300

    flag evaluator tests

---
+++ tests/FfaasLite.Tests/FlagEvaluatorGeneralFlagTests.cs
@@ -0,0 +1,85 @@
+using FfaasLite.Core.Flags;
+using FfaasLite.Core.Models;
+using System.Diagnostics.Metrics;
+
+namespace FfaasLite.Tests
+{
+    public class FlagEvaluatorGeneralFlagTests
+    {
+        [Fact]
+        public void Priority_Null_Rules_Run_Last()
+        {
+            var flag = new Flag
+            {
+                Key = "k",
+                Type = FlagType.Boolean,
+                BoolValue = false,
+                Rules =
+                [
+                    new TargetRule("country", "eq", "NL", Priority: null, BoolOverride: false),
+                    new TargetRule("country", "eq", "NL", Priority: 1,    BoolOverride: true),
+                ]
+            };
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: new() { ["country"] = "NL" });
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.True((bool)res.Value!);  // сработало правило с приоритетом 1
+            Assert.Equal("rule", res.Variant);
+        }
+
+        [Fact]
+        public void Attribute_Not_Present_Does_Not_Match()
+        {
+            var flag = new Flag
+            {
+                Key = "k",
+                Type = FlagType.Boolean,
+                BoolValue = true,
+                Rules =
+                [
+                    new TargetRule("missingAttr", "eq", "X", Priority: 1, BoolOverride: false)
+                ]
+            };
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: new());
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.True((bool)res.Value!);
+            Assert.Equal("default", res.Variant);
+        }
+
+        [Theory]
+        [InlineData("eq", "NL", "nl", true)]
+        [InlineData("ne", "NL", "de", true)]
+        [InlineData("ne", "NL", "NL", false)]
+        [InlineData("contains", "oO", "foo", true)]
+        public void Operators_Work_CaseInsensitive(string op, string ruleVal, string actual, bool shouldMatch)
+        {
+            var flag = new Flag
+            {
+                Key = "k",
+                Type = FlagType.Boolean,
+                BoolValue = false,
+                Rules = [new("attr", op, ruleVal, Priority: 1, BoolOverride: true)]
+            };
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: new() { ["attr"] = actual });
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            if (shouldMatch)
+            {
+                Assert.True((bool)res.Value!);
+                Assert.Equal("rule", res.Variant);
+            }
+            else
+            {
+                Assert.False((bool)res.Value!);
+                Assert.Equal("default", res.Variant);
+            }
+        }
+    }
+}
---
+++ tests/FfaasLite.Tests/FlagEvaluatorStringFlagTests.cs
@@ -0,0 +1,103 @@
+using FfaasLite.Core.Flags;
+using FfaasLite.Core.Models;
+using System.Diagnostics.Metrics;
+
+namespace FfaasLite.Tests
+{
+    public class FlagEvaluatorStringFlagTests
+    {
+        private static Flag MakeBoolFlag(bool @default, params TargetRule[] rules) => new()
+        {
+            Key = "bool-flag",
+            Type = FlagType.Boolean,
+            BoolValue = @default,
+            Rules = [.. rules]
+        };
+
+        [Fact]
+        public void Match_By_Attribute_Returns_Override_True()
+        {
+            var flag = MakeBoolFlag(false,
+                new TargetRule("country", "eq", "NL", Priority: 1, BoolOverride: true));
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: new() { ["country"] = "NL" });
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.True((bool)res.Value!);
+            Assert.Equal("rule", res.Variant);
+        }
+
+        [Fact]
+        public void No_Match_Returns_Default()
+        {
+            var flag = MakeBoolFlag(true,
+                new TargetRule("country", "eq", "NL", Priority: 1, BoolOverride: false));
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: new() { ["country"] = "DE" });
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.True((bool)res.Value!);
+            Assert.Equal("default", res.Variant);
+        }
+
+        [Fact]
+        public void Match_By_UserId_Special_Attribute()
+        {
+            string userId = Guid.NewGuid().ToString();
+
+            var flag = MakeBoolFlag(false,
+                new TargetRule("userId", "eq", userId, Priority: 1, BoolOverride: true));
+
+            var ctx = new EvalContext(UserId: userId, Attributes: []);
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.True((bool)res.Value!);
+            Assert.Equal("rule", res.Variant);
+        }
+
+        [Fact]
+        public void First_Matching_Rule_Wins_By_Priority()
+        {
+            var flag = MakeBoolFlag(false,
+                new TargetRule("country", "eq", "NL", Priority: 2, BoolOverride: false),
+                new TargetRule("country", "eq", "NL", Priority: 1, BoolOverride: true));
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: new() { ["country"] = "NL" });
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.True((bool)res.Value!);
+            Assert.Equal("rule", res.Variant);
+        }
+
+        [Fact]
+        public void Empty_Rules_List_Always_Returns_Default()
+        {
+            var flag = MakeBoolFlag(true /* default */);
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: []);
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.True((bool)res.Value!);
+            Assert.Equal("default", res.Variant);
+        }
+
+        [Fact]
+        public void Wrong_Override_Type_Is_Ignored_Falls_Back_To_Default()
+        {
+            var flag = MakeBoolFlag(false,
+                new TargetRule("country", "eq", "NL", Priority: 1, StringOverride: "oops"));
+
+            var ctx = new EvalContext(UserId: "u1", Attributes: new() { ["country"] = "NL" });
+
+            var res = new FlagEvaluator().Evaluate(flag, ctx);
+
+            Assert.False((bool)res.Value!);     // BoolOverride is null, take default
+            Assert.Equal("rule", res.Variant);  // rule, but without override
+        }
+    }
+}
